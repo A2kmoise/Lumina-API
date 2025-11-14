f54c462964dd68968e220cce1038dc21bc35e521
A2kmoise <abayomoise950@gmail.com>
2025-11-14 12:13:25 +0200

    transporter auth done

--- src/transporter/transporter.controller.ts
+++ src/transporter/transporter.controller.ts
@@ -1,22 +1,28 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { TransporterService } from "./transporter.service";
+import { Sign } from "crypto";
+import { LoginDto, SignupDto } from "./dto";
 
 @Controller('transporter')
 export class TransporterController {
   constructor(private transporterService: TransporterService) {}
 
   @Post('signup')
-  signup() {
-    return this.transporterService.signUp();
+  signup(@Body() dto: SignupDto) {
+    return this.transporterService.signUp(dto);
   }
 
   @Post('login')
-  login() {
-    return this.transporterService.login();
+  login(@Body() dto:LoginDto) {
+    return this.transporterService.login(dto);
+  }
+@Post('refresh')
+  refresh(@Body('refreshToken') refreshToken: string) {
+    return this.transporterService.refreshTokens(refreshToken);
   }
 
   @Post('logout')
-  logout() {
-    return this.transporterService.logout();
+  logout(@Body('transporterId') transporterId: string) {
+    return this.transporterService.logout(transporterId);
   }
 }
