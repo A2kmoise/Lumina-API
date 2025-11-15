ca81277081ef439cb9d1752430dd38938707affb
A2kmoise <abayomoise950@gmail.com>
2025-11-15 11:17:03 +0200

    transporter module configured well

--- src/transporter/transporter.module.ts
+++ src/transporter/transporter.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TransporterService } from './transporter.service';
+import { TransporterController } from './transporter.controller';
+import { TransporterAuthModule } from './auth/transporter-auth.module';
 
 @Module({
+  imports: [TransporterAuthModule],
+  controllers: [TransporterController],
   providers: [TransporterService]
 })
 export class TransporterModule {}
