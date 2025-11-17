3a7e6c065a442af8d7a76f730b9f20a80a04e5f7
A2kmoise <abayomoise950@gmail.com>
2025-11-17 16:31:53 +0200

    transporter profile setup

--- src/transporter/transporter.module.ts
+++ src/transporter/transporter.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TransporterService } from './transporter.service';
 import { TransporterController } from './transporter.controller';
 import { TransporterAuthModule } from './auth/transporter-auth.module';
+import { ProfileController } from './profile/profile.controller';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [TransporterAuthModule],
-  controllers: [TransporterController],
+  imports: [TransporterAuthModule, ProfileModule],
+  controllers: [TransporterController, ProfileController],
   providers: [TransporterService]
 })
 export class TransporterModule {}
